7078ce4958ebb9d802abd114191f4f4b7685858c
tehjul <sibillej@gmail.com>
2021-04-11 10:36:50 +0200

    last version of multiply function

--- td2/src/mathematics/Maths.java
+++ td2/src/mathematics/Maths.java
@@ -17,19 +17,36 @@ public class Maths implements IMaths {
 
 	@Override
 	public int multiplication(int a, int b) {
-		
-		 int resultat = 0;
-		 
-		 if (a < b) { 
-			 for (int i=0; i < Math.abs(a); i++) { 
-				 resultat = addition(resultat, b); 
-				 }
-		 } else { 
-			 for (int i=0; i < Math.abs(b); i++) { 
-				 resultat = addition(resultat, a); }
-			 }
-		  
-		 return resultat;
+
+		int resultat = 0;
+
+		if (a<0 && b<0) {
+			if (Math.abs(a) < Math.abs(b)) {
+				for (int i=0; i<Math.abs(a); i++) {
+					resultat = addition(resultat, -b);
+				}
+			} else {
+				for (int i=0; i<Math.abs(b); i++) {
+					resultat = addition(resultat, -a);
+				}
+			}
+
+		} else {
+			
+			if (a < 0) {
+				for (int i=0; i<Math.abs(b); i++) {
+					resultat = addition(resultat, a);
+				}
+			} else {
+				for (int i=0; i<Math.abs(a); i++) {
+					resultat = addition(resultat, b);
+				}
+			}
+
+
+		} 
+
+		return resultat;
 		 
 	}
 
