0892b1682b4f364e9394828a1d31fa8360c1acd9
tehjul <sibillej@gmail.com>
2021-04-18 11:05:39 +0200

    correction

---
+++ calculatrice/src/main/java/com/iut/as2021/enumerations/EOperation.java
@@ -0,0 +1,33 @@
+package com.iut.as2021.enumerations;
+
+public enum EOperation {
+    INCONNUE(0, ""),
+    ADDITION(1, "+"),
+    SOUSTRACTION(2, "-"),
+    MULTIPLICATION(3, "*"),
+    DIVISION(4, "/");
+
+    private int code;
+    private String operateur;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getOperateur() {
+        return operateur;
+    }
+
+    public void setOperateur(String operateur) {
+        this.operateur = operateur;
+    }
+
+    private EOperation(int code, String operateur) {
+        this.code = code;
+        this.operateur = operateur;
+    }
+}
---
+++ calculatrice/src/test/java/com/iut/as2021/metier/MathResultatTest.java
@@ -0,0 +1,34 @@
+package com.iut.as2021.metier;
+
+import com.iut.as2021.exceptions.MathsExceptions;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MathResultatTest {
+    private MathResultat resultat;
+
+    @Test
+    public void testExpressionSimpleAddition() throws MathsExceptions {
+        resultat = new MathResultat("2 + 3 - 1 + 7 + 8");
+        assertEquals(resultat.calculate(), 19, 2);
+    }
+
+    @Test
+    public void testExpressionSimpleSoustraction() throws MathsExceptions {
+        resultat = new MathResultat("7 - 3 + 1");
+        assertEquals(resultat.calculate(), 5, 2);
+    }
+
+    @Test
+    public void testExpressionSimpleMultiplication() throws MathsExceptions {
+        resultat = new MathResultat("7 * 3");
+        assertEquals(resultat.calculate(), 21, 2);
+    }
+
+    @Test
+    public void testExpressionSimpleDivision() throws MathsExceptions {
+        resultat = new MathResultat("8 / 2");
+        assertEquals(resultat.calculate(), 4, 2);
+    }
+}
