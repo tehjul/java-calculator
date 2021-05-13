11637deae23615c6c10f73aa3767fe3581564207
tehjul <sibillej@gmail.com>
2021-05-13 21:47:17 +0200

    override IDaoMathResult

--- calculatrice/src/main/java/com/iut/as2021/dao/IDaoMathResult.java
+++ calculatrice/src/main/java/com/iut/as2021/dao/IDaoMathResult.java
@@ -2,6 +2,21 @@ package com.iut.as2021.dao;
 
 import com.iut.as2021.metier.MathResultat;
 
-public interface IDaoMathResult extends IDao<MathResultat> {
+import java.util.List;
 
+public interface IDaoMathResult extends IDao<MathResultat> {
+    @Override
+    MathResultat readyById(int i);
+
+    @Override
+    List<MathResultat> getAll();
+
+    @Override
+    boolean update(MathResultat object);
+
+    @Override
+    boolean create(MathResultat object);
+
+    @Override
+    boolean delete(MathResultat object);
 }
